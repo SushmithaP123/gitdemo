b3a300bdfa4b0baf065405b9dccff3077dfa7a5f
sushmip <sushmip@inlen8520087981.corp.capgemini.com>
2023-11-22 17:49:48 +0530

    hi

--- Student/src/main/java/com/student/service/ServiceImp.java
+++ Student/src/main/java/com/student/service/ServiceImp.java
@@ -2,6 +2,8 @@ package com.student.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,12 @@ public class ServiceImp implements StudentService{
 	@Autowired
 	private StudentRepository studentRepository;
 
+private static final Logger log = LoggerFactory.getLogger(ServiceImp.class);
+
 	@Override
 	public Student addstudent(Student student) {
 		studentRepository.save(student);
+		log.info("student added");	
 		return student;
 	}
 
